923018eec04a04f69f7989be7359a1ae6666a514
SANDO-07 <csandovals5@miumg.edu.gt>
2024-04-26 22:42:23 -0600

    NUEVAS FORMAS DE DIBUJAR

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 
 using namespace std;
 
-const int Ancho_pantalla = 80;
-const int Alto_pantalla = 35;
+const int Ancho_pantalla = 50;
+const int Alto_pantalla = 25;
 const char Dibujo = '*';
-const char Puntero = 'X';
+const char Puntero = '+';
 
 int CoordenadaX = Ancho_pantalla / 2;
 int CoordenadaY = Alto_pantalla / 2;
@@ -39,6 +39,8 @@ vector<modeloCirculo> circulos;
 vector<modeloTriangulo> triangulos;
 vector<modeloRectangulo> rectangulos;
 
+bool quit = false;
+
 void gotoxy(int x, int y, char c) {
     COORD coord;
     coord.X = x;
@@ -50,17 +52,51 @@ void gotoxy(int x, int y, char c) {
 
 void dibujar_cuadrado(modeloCuadrado cuadrado) {
     int size = cuadrado.lado;
-    int x = cuadrado.coord.X;
-    int y = cuadrado.coord.Y;
+    int x = (cuadrado.coord.X + Ancho_pantalla) % Ancho_pantalla;
+    int y = (cuadrado.coord.Y + Alto_pantalla) % Alto_pantalla;
 
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
             if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
                 COORD coord;
-                coord.X = (x - size / 2 + j + Ancho_pantalla) % Ancho_pantalla;
-                coord.Y = (y - size / 2 + i + Alto_pantalla) % Alto_pantalla;
+                coord.X = (x + j + Ancho_pantalla) % Ancho_pantalla;
+                coord.Y = (y + i + Alto_pantalla) % Alto_pantalla;
                 SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-                SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE), 4);
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+                cout << Dibujo;
+            }
+        }
+    }
+}
+
+void dibujar_triangulo(int base, int x, int y) {
+    int altura = (base + 1) / 2;
+
+    int startX = (x - (altura - 1) + Ancho_pantalla) % Ancho_pantalla;
+
+    for (int i = 0; i < altura; ++i) {
+        for (int j = 0; j < 2 * altura - 1; ++j) {
+            COORD coord;
+            coord.X = (startX + j + Ancho_pantalla) % Ancho_pantalla;
+            coord.Y = (y + i + Alto_pantalla) % Alto_pantalla;
+            if ((j == altura - i - 1) || (j == altura + i - 1) || (i == altura - 1)) {
+                SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+                cout << Dibujo;
+            }
+        }
+    }
+}
+
+void dibujar_rectangulo(int ancho, int altura, int x, int y) {
+    for (int i = 0; i < altura; ++i) {
+        for (int j = 0; j < ancho; ++j) {
+            COORD coord;
+            coord.X = (x + j + Ancho_pantalla) % Ancho_pantalla;
+            coord.Y = (y + i + Alto_pantalla) % Alto_pantalla;
+            if (i == 0 || i == altura - 1 || j == 0 || j == ancho - 1) {
+                SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
                 cout << Dibujo;
             }
         }
@@ -81,43 +117,12 @@ void dibujar_circulo(int x, int y, int radio, char c) {
         }
     }
 }
-void dibujar_triangulo(int base, int x, int y) {
-    int height = base / 2 + 1;
 
-    int startX = x - base / 2;
-    int startY = y + height - 1;
-
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < base; ++j) {
-            COORD coord;
-            coord.X = (startX + j + Ancho_pantalla) % Ancho_pantalla;
-            coord.Y = (startY - i + Alto_pantalla) % Alto_pantalla;
-            if (j >= i && j < base - i) {
-                if (i == 0 || j == i || j == base - i - 1 || i == height - 1) {
-                    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-                    cout << Dibujo;
-                }
-            }
-        }
-    }
-}
-void dibujar_rectangulo(int width, int height, int x, int y) {
-    COORD coord;
-    coord.X = (x - width / 2 + Ancho_pantalla) % Ancho_pantalla;
-    coord.Y = (y - height / 2 + Alto_pantalla) % Alto_pantalla;
-
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
-                coord.X = (x - width / 2 + j + Ancho_pantalla) % Ancho_pantalla;
-                coord.Y = (y - height / 2 + i + Alto_pantalla) % Alto_pantalla;
-                SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-                SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE), 2);
-                cout << Dibujo;
-            }
-        }
-    }
+void borrar_dibujos (){
+     rectangulos.clear();
+     circulos.clear();
+     triangulos.clear();
+     cuadrados.clear();
 }
 void limpiar_pantalla() {
     system("cls");
@@ -126,13 +131,14 @@ int Menu () {
     limpiar_pantalla();
     system("color F1");
     int choice;
+    cout << "\n\n \t\t Mientras este dibujando presione la tecla c para borrar todas las figuras en pantalla ";
     cout << "\n\n\t\t\t--------------------------------------------------------------------------";
     cout << "\n\t\t\t| \t Bienvenido a programa mi UMG Seleccione una opcion: \t\t | \n";
     cout << "\t\t \t| 1. Triangulo | ";
     cout << " 2. Cuadrado |";
     cout << " 3. Rectangulo |";
     cout << " 4. Circulo |";
-    cout << " 5. Esc \t | \n";
+    cout << " 9. Salir \t | \n";
     cout << "\t\t\t--------------------------------------------------------------------------";
     cout << "\n\n \t\t ingrese su eleccion: ";
     cin >> choice;
@@ -140,7 +146,7 @@ int Menu () {
     switch (choice) {
         case 1: {
             int base;
-            cout << "Ingrese el tamanio de la base del triangulo: ";
+            cout << "\n *Ingrese el tamanio de la base del triangulo: ";
             cin >> base;
             modeloTriangulo nuevoTriangulo;
             nuevoTriangulo.base = base;
@@ -151,7 +157,7 @@ int Menu () {
         }
         case 2: {
             int lado;
-            cout << "Ingrese el tamanio del lado del cuadrado: ";
+            cout << "\n *Ingrese el tamanio del lado del cuadrado: ";
             cin >> lado;
             modeloCuadrado nuevoCuadrado;
             nuevoCuadrado.lado = lado;
@@ -162,9 +168,9 @@ int Menu () {
         }
         case 3: {
             int ancho, alto;
-            cout << "Ingrese el ancho del rectangulo: ";
+            cout << "\n *Ingrese el ancho del rectangulo: ";
             cin >> ancho;
-            cout << "Ingrese el alto del rectangulo: ";
+            cout << "\n *Ingrese el alto del rectangulo: ";
             cin >> alto;
             modeloRectangulo nuevoRectangulo;
             nuevoRectangulo.ancho = ancho;
@@ -176,7 +182,7 @@ int Menu () {
         }
         case 4: {
             int radio;
-            cout << "Ingrese el radio del circulo: ";
+            cout << "\n *Ingrese el radio del circulo: ";
             cin >> radio;
             modeloCirculo nuevoCirculo;
             nuevoCirculo.radio = radio;
@@ -221,7 +227,7 @@ int main() {
     info.bVisible = FALSE;
     SetConsoleCursorInfo(console, &info);
 
-    bool quit = false;
+
     while (!quit) {
         limpiar_pantalla();
         cargarCuadrados();
@@ -245,7 +251,10 @@ int main() {
                 case 's':
                     CoordenadaY = (CoordenadaY + 1 + Alto_pantalla) % Alto_pantalla;
                     break;
-                case VK_ESCAPE:
+                    case 'c':
+                    borrar_dibujos();
+                    break;
+                case '9':
                     quit = true;
                     break;
                 default:
